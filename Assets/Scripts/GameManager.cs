0a1d04bcb20cafce01b88721eae5ce9610351df8
sebaso <sebastian.olaechea.so@gmail.com>
2026-02-05 13:23:47 +0100

    h

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
     private PlayerController playerController;
     private PlayerShootyManager playerShootyManager;
     private WeaponHolder weaponHolder;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         playerController = PlayerController.instance;
